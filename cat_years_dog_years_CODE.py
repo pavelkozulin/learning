3aa41d5cff40fdcb089aabaaca01f00aa3880c1c
pavelkozulin <73241099+pavelkozulin@users.noreply.github.com>
2020-10-23 12:47:21 +0300

    Add files via upload

---
+++ cat_years_dog_years_CODE.py
@@ -0,0 +1,13 @@
+1def human_years_cat_years_dog_years(human_years):
+    if human_years == 1:
+        cat_years = dog_years = 15
+    elif human_years == 2:
+        cat_years = dog_years = 24
+    else:
+        cat_years = 24 + (human_years - 2) * 4
+        dog_years = 24 + (human_years - 2) * 5
+    return(human_years, cat_years, dog_years)
+
+human_years_cat_years_dog_years(1)
+human_years_cat_years_dog_years(2)
+human_years_cat_years_dog_years(10)
