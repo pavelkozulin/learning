ae288a365c3d24ddfe4fd90f6fcc04f7988f18e8
pavelkozulin <73241099+pavelkozulin@users.noreply.github.com>
2021-01-13 00:16:53 +0300

    Add files via upload

--- alphabet_war.py
+++ alphabet_war.py
@@ -3,16 +3,17 @@ def fight(text):
     >>> fight('')
     "Let's fight again!"
     >>> fight('abracadabra')
-    'Left side wins!'
+    "Left side wins!"
     >>> fight('z')
-    'Right side wins!'
+    "Right side wins!"
     >>> fight('zdqmwpbs')
     "Let's fight again!"
     >>> fight('zzzzs')
-    'Right side wins!'
+    "Right side wins!"
     >>> fight('wwwwwwz')
-    'Left side wins!'
+    "Left side wins!"
     """
+    
     war_dict = {'w': 4, 'p': 3, 'b': 2, 's': 1, 'm': -4, 'q': -3, 'd': -2, 'z': -1}
     a = [x for x in text]
     b = 0
@@ -20,9 +21,9 @@ def fight(text):
         if y in war_dict:
             b += war_dict[y]
     if b > 0:
-        result = "Left side wins"
+        result = "Left side wins!"
     elif b < 0:
-        result = "Right side wins"
+        result = "Right side wins!"
     else:
         result = "Let's fight again!"
     return result
@@ -32,3 +33,7 @@ fight('z')
 fight('zdqmwpbs')
 fight('zzzzs')
 fight('wwwwwwz')
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
