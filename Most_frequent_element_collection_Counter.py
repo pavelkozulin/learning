397115f186f56684e26ff7c0021d1008e5a4b97d
pavelkozulin <73241099+pavelkozulin@users.noreply.github.com>
2021-02-14 18:13:06 +0300

    Add files via upload

---
+++ Most_frequent_element_collection_Counter.py
@@ -0,0 +1,47 @@
+def Most_ferquent_element(array):
+    '''
+    Дан массив a из n целых чисел. Напишите программу, которая найдет число, которое чаще других встречается в массиве.
+    Ограничение времени: 2 с, ограничение памяти: 256 МБ.
+
+    Формат ввода
+    В первой строке входных данных записано число n (1 ≤ n ≤ 300 000).
+    Во второй строке записаны n целых чисел ai (0 ≤ ai ≤ 1 000 000 000).
+
+    Формат вывода
+    Выведите единственное число x, наибольшее из чисел, которое чаще других встречается в массиве a.
+
+    >>> Most_ferquent_element((1,1,2))
+    1
+    >>> Most_ferquent_element((1,1,2,2))
+    2
+    >>> Most_ferquent_element((2,2,1,1))
+    2
+    >>> Most_ferquent_element((1,2,3))
+    3
+    >>> Most_ferquent_element((0))
+    0
+    >>> Most_ferquent_element((-1,1,-1))
+    -1
+
+    '''
+
+    if len(str(array)) == 1:
+        return array
+    import collections
+    element_frequency_dict = collections.Counter()
+    for i in array:
+        element_frequency_dict[i] += 1
+    max_count = 1
+    max_element = 1
+
+    for i in element_frequency_dict:
+        if element_frequency_dict[i] > max_count:
+            max_count = element_frequency_dict[i]
+            max_element = i
+        if element_frequency_dict[i] == max_count and max_element < i:
+            max_element = i
+    return max_element
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
