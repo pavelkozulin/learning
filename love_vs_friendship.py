1d39b6a51f291d2180659036306fe3df82e33dd5
pavelkozulin <73241099+pavelkozulin@users.noreply.github.com>
2021-02-18 15:49:02 +0300

    Add files via upload

---
+++ love_vs_friendship.py
@@ -0,0 +1,26 @@
+import string
+def words_to_marks(word):
+    """
+    >>> words_to_marks('attitude')
+    100
+    >>> words_to_marks('friends')
+    75
+    >>> words_to_marks('family')
+    66
+    >>> words_to_marks('selfness')
+    99
+    >>> words_to_marks('knowledge')
+    96
+    """
+
+    letters = string.ascii_lowercase
+    numbers = [i for i in range(1, 27)]
+    letter_number_dict = dict(zip(letters, numbers))
+
+
+    return sum([letter_number_dict[i] for i in word])
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
