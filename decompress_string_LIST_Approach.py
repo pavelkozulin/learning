4c68c17e58ae382d23eaf4e89627d5faec79a60f
pavelkozulin <73241099+pavelkozulin@users.noreply.github.com>
2021-03-02 18:49:37 +0300

    Add files via upload

---
+++ decompress_string_LIST_Approach.py
@@ -0,0 +1,44 @@
+def decompress_string(text):
+    """
+    Write a inverse function.
+
+    >>> decompress_string('')
+    ''
+    >>> decompress_string('a10b')
+    'aaaaaaaaaab'
+    >>> decompress_string('a5ba5')
+    'aaaaabaaaaa'
+    >>> decompress_string('a3b4cd3')
+    'aaabbbbcddd'
+    """
+
+    result = []
+    last_letter = ''
+    buffer = []
+
+    for i in text:
+        #print('i = ', i)
+        if i.isalpha():
+            #print('isalpha')
+            if buffer == []:
+                result.append(last_letter)
+            else:
+                result.append(last_letter * (int(''.join(buffer))))
+            #print('result: ', result)
+            last_letter = i
+            buffer = []
+        else:
+            #print('isdigit')
+            buffer.append(i)
+            #print('digit_gather: ', digit_gather)
+    if buffer == []:
+        result.append(last_letter)
+    else:
+        result.append(last_letter * (int(''.join(buffer))))
+    #print('result: ', result)
+    final = ''.join(result)
+    return final 
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
